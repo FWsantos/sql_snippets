dcec8ae6e064313d6e989a6304f36ccd74419397
FWSantos <fwsantos23@gmail.com>
2019-12-03 10:30:58 -0300

    'update'

--- select_tables.sql
+++ select_tables.sql
@@ -3,5 +3,7 @@ from operacao_efetuada join tipo_operacao on tipo_operacao.id = id_operacao;
 
 
 
+select id_conta, count(id_conta) from operacao_efetuada GROUP BY id_conta order by count(id_conta);
+
 
 
